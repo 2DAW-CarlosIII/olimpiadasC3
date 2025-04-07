6413ee9765759945b382fa3642c72f165e03191d
xenarff <xenarff>
2025-04-07 02:58:38 +0200

    dividido en partials

---
+++ resources/views/readonly/partials/header.blade.php
@@ -0,0 +1,24 @@
+<section id="header">
+    <header>
+        <span class="image avatar"><img src="{{ asset('/readonly/assets/images/c3_logo.png') }}" alt="" /></span>
+        <h1 id="logo"><a href="#">Olimpiadas Informáticas Región de Murcia</a></h1>
+    </header>
+    <nav id="nav">
+        <ul>
+            <li><a href="#one" class="active">Objetivos</a></li>
+            <li><a href="#two">Ediciones</a></li>
+            <li><a href="#three">Inscripciones</a></li>
+            <li><a href="#four">Resultados</a></li>
+            <li><a href="#five">Patrocinadores</a></li>
+        </ul>
+    </nav>
+    <footer>
+        <ul class="icons">
+            <li><a href="#" class="icon brands fa-twitter"><span class="label">Twitter</span></a></li>
+            <li><a href="#" class="icon brands fa-facebook-f"><span class="label">Facebook</span></a></li>
+            <li><a href="#" class="icon brands fa-instagram"><span class="label">Instagram</span></a></li>
+            <li><a href="#" class="icon brands fa-github"><span class="label">Github</span></a></li>
+            <li><a href="#" class="icon solid fa-envelope"><span class="label">Email</span></a></li>
+        </ul>
+    </footer>
+</section>
---
+++ resources/views/readonly/partials/main.blade.php
@@ -0,0 +1,88 @@
+<div id="main">
+    <!-- One -->
+        <section id="one">
+            <div class="image main" data-position="center">
+                <img src="{{ asset('/readonly/assets/images/principal.avif') }}" alt="" />
+            </div>
+            <div class="container">
+                <header class="major">
+                    <h2>Objetivos</h2>
+                </header>
+                <p>Las Olimpiada Informáticas de la Región de Murcia para los Institutos de Educación Secundaria y
+                    Centros Integrados de Formación Profesional tiene como objetivos:</p>
+                <p>• el fomento del trabajo en equipo, propiciando que los alumnos focalicen sus esfuerzos en conseguir
+                    metas comunes, potenciando la sinergia que les lleve a obtener soluciones más rápidas y eficientes
+                    que de forma individual. <br>
+                    • El impulso de los estudios de Formación Profesional, particularmente de Informática, los cuales
+                    tienen como finalidad la preparación de los alumnos para la actividad en este campo profesional. La
+                    Formación Profesional proporciona a sus alumnos un aprendizaje polivalente, que les permite
+                    adaptarse a las modifcaciones laborales que pueden producirse a lo largo de su vida.</p>
+            </div>
+        </section>
+    <!-- Two -->
+        <section id="two">
+            <div class="container">
+                <h3>Ediciones</h3>
+                <p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer non. Adipiscing cubilia elementum integer lorem ipsum dolor sit amet.</p>
+                <ul class="feature-icons">
+                    <li class="icon solid fa-code">Write all the code</li>
+                    <li class="icon solid fa-cubes">Stack small boxes</li>
+                    <li class="icon solid fa-book">Read books and stuff</li>
+                    <li class="icon solid fa-coffee">Drink much coffee</li>
+                    <li class="icon solid fa-bolt">Lightning bolt</li>
+                    <li class="icon solid fa-users">Shadow clone technique</li>
+                </ul>
+            </div>
+        </section>
+    <!-- Three -->
+        <section id="three">
+            <div class="container">
+                <h3>Inscripciones</h3>
+                <p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer non. Adipiscing cubilia elementum integer. Integer eu ante ornare amet commetus.</p>
+                <form method="post" action="#">
+                    <div class="row gtr-uniform">
+                        <div class="col-6 col-12-xsmall"><input type="text" name="name" id="name" placeholder="Name" /></div>
+                        <div class="col-6 col-12-xsmall"><input type="email" name="email" id="email" placeholder="Email" /></div>
+                        <div class="col-12"><input type="text" name="subject" id="subject" placeholder="Subject" /></div>
+                        <div class="col-12"><textarea name="message" id="message" placeholder="Message" rows="6"></textarea></div>
+                        <div class="col-12">
+                            <ul class="actions">
+                                <li><input type="submit" class="primary" value="Send Message" /></li>
+                                <li><input type="reset" value="Reset Form" /></li>
+                            </ul>
+                        </div>
+                    </div>
+                </form>
+            </div>
+        </section>
+    <!-- Five -->
+        <section id="five">
+            <div class="container">
+                <h3>Patrocinadores</h3>
+                <p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer non. Adipiscing cubilia elementum integer. Integer eu ante ornare amet commetus.</p>
+                <div class="features">
+                    <article>
+                        <a href="#" class="image"><img src="{{ asset('/readonly/images/pic01.jpg') }}" alt="" /></a>
+                        <div class="inner">
+                            <h4>Possibly broke spacetime</h4>
+                            <p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer adipiscing ornare amet.</p>
+                        </div>
+                    </article>
+                    <article>
+                        <a href="#" class="image"><img src="{{ asset('/readonly/images/pic02.jpg') }}" alt="" /></a>
+                        <div class="inner">
+                            <h4>Terraformed a small moon</h4>
+                            <p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer adipiscing ornare amet.</p>
+                        </div>
+                    </article>
+                    <article>
+                        <a href="#" class="image"><img src="{{ asset('/readonly/images/pic03.jpg') }}" alt="" /></a>
+                        <div class="inner">
+                            <h4>Snapped dark matter in the wild</h4>
+                            <p>Integer eu ante ornare amet commetus vestibulum blandit integer in curae ac faucibus integer adipiscing ornare amet.</p>
+                        </div>
+                    </article>
+                </div>
+            </div>
+        </section>
+</div>
